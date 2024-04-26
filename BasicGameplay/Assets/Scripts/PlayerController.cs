60c0b530ecd6e6d0e89e7e919a3854a36aa17591
Timothy Ryden <timothygryden@gmail.com>
2024-04-26 13:31:04 -0400

    3873

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -17,16 +17,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -10.0f)
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10.0f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x < 10.0f)
+        if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(10.0f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-            horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
