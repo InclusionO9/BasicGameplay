6d95450b6e155052b1be5fc7e663596fb081bef1
Timothy Ryden <timothygryden@gmail.com>
2024-05-01 14:06:04 -0400

    8493

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -25,9 +25,10 @@ public class PlayerController : MonoBehaviour
 
         if (transform.position.x > xRange)
         {
+
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        horizontalInput = Input.GetAxis("Horizontal");
+            horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
